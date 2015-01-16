345209ca5faa416b4aab33187a48eb425a6ac848
filipoff <marto_1337@hotmail.com>
2015-01-16 23:47:47 +0200

    added board

--- MazeSolver/Cell.cpp
+++ MazeSolver/Cell.cpp
@@ -1,8 +1,35 @@
 #include "Cell.h"
 
+Cell::Cell()
+{
+	symbol = 0;
+	row = 0;
+	column = 0;
+}
+
 Cell::Cell(char symbol, int row, int column) :
 symbol(symbol), row(row), column(column)
 {}
+
+
+Cell::Cell(const Cell& other)
+{
+	copyFrom(other);
+}
+
+Cell& Cell::operator=(const Cell& other)
+{
+	if (this != &other)
+		copyFrom(other);
+	return *this;
+}
+
+void Cell::copyFrom(const Cell& other)
+{
+	symbol = other.symbol;
+	row = other.row;
+	column = other.column;
+}
 
 char Cell::getSymbol() const
 {
